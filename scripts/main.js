8efd500a02f67e4628966becf510dc3ce49990fc
Lejden Lazaro <lejdenlazaro@gmail.com>
2025-03-26 00:01:02 +0800

    Save progress for string methods

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,19 @@
 "use strict";
 
-const string = "0123456789";
-console.log(`length of string: ${string.length}`);
-console.log(`sliced string: ${string.slice()}`);
+const number = "0123456789";
+const alphabet = "ABCDEFGHIJKLMnopqrstuvwxyz";
+
+console.log(`number: ${number}`);
+console.log(`number.length: ${number.length} type: ${typeof number.length}`);
+console.log(
+  `number.sliced(): ${number.slice(5)} type: ${typeof number.slice(5)}`
+);
+console.log(
+  `number.substring(): ${number.substring(8, 9)} type: ${typeof number.slice(
+    5
+  )}`
+);
+
+console.log(`alphabet: ${alphabet}`);
+console.log(alphabet.toUpperCase());
+console.log(alphabet.toLowerCase());
