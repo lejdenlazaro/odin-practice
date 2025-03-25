c4f81f7d15fabc47cb7afce25c13bcd94366d565
Lejden Lazaro <lejdenlazaro@gmail.com>
2025-03-25 12:40:19 +0800

    Save progress for string practice

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,7 @@
-let age;
-alert(age);
+"use strict";
+
+const stringOne = "one";
+const stringTwo = "two";
+const stringThree = "three";
+const total = `${stringOne}+${stringTwo}=${stringThree}`;
+console.log(total);
