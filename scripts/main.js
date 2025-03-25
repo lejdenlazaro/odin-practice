fc9117fdaad08a8402ed915d0b5ec38935878dc1
Lejden Lazaro <lejdenlazaro@gmail.com>
2025-03-26 00:32:56 +0800

    Save progress for pad string methods

--- scripts/main.js
+++ scripts/main.js
@@ -2,6 +2,8 @@
 
 const number = "0123456789";
 const alphabet = "ABCDEFGHIJKLMnopqrstuvwxyz";
+const name = "        Lejden        Lazaro        ";
+const phone = "9162024972";
 
 console.log(`number: ${number}`);
 console.log(`number.length: ${number.length} type: ${typeof number.length}`);
@@ -17,3 +19,14 @@ console.log(
 console.log(`alphabet: ${alphabet}`);
 console.log(alphabet.toUpperCase());
 console.log(alphabet.toLowerCase());
+
+console.log(number.concat(alphabet, alphabet, alphabet));
+
+console.log(`name: ${name}`);
+console.log(`name.trim(): ${name.trim()}`);
+console.log(`name.trimStart(): ${name.trimStart()}`);
+console.log(`name.trimEnd(): ${name.trimEnd()}`);
+
+console.log(`phone: ${phone}`);
+console.log(`phone.padStart(): ${phone.padStart(phone.length + 5 * 3, "+63")}`);
+console.log(`phone.padEnd(): ${phone.padEnd(100, "foo")}`);
