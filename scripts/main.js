0e2c9ddf80dd3767e0f5ef626f13728d20687d4e
Lejden Lazaro <lejdenlazaro@gmail.com>
2025-04-02 16:05:05 +0800

    Fix output format

--- scripts/main.js
+++ scripts/main.js
@@ -169,9 +169,9 @@ const button = document.querySelector("button");
 
 function fizzBuzz(num) {
   for (let i = 1; i <= num; i++) {
-    if (i % 3 == 0 && i % 5 == 0) console.log("fizzbuzz");
-    else if (i % 3 == 0) console.log("fizz");
-    else if (i % 5 == 0) console.log("buzz");
+    if (i % 3 == 0 && i % 5 == 0) console.log("FizzBuzz");
+    else if (i % 3 == 0) console.log("Fizz");
+    else if (i % 5 == 0) console.log("Buzz");
     else console.log(i);
   }
 }
