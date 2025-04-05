d4994979008c93309cfa6c4b533414a4ca592cf0
Lejden Lazaro <lejdenlazaro@gmail.com>
2025-04-05 14:54:10 +0800

    Save progress array

--- scripts/main.js
+++ scripts/main.js
@@ -3,3 +3,17 @@
 const div = document.querySelector("div");
 div.textContent = "🖕🏻";
 div.style.fontSize = "150px";
+
+const veggies = ["kangkong", "malunggay", "okra", "talong"];
+const veggiesSize = veggies.length;
+console.log(veggiesSize);
+
+console.log(veggies.toString());
+
+function someFunction() {
+  return "return someFunction";
+}
+
+const i = 0;
+veggies[i] = "impostor veggie";
+console.log(veggies[i]);
