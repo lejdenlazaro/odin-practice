5ff4fa5c9f6167dd5923d097970af45f625302ce
Lejden Lazaro <lejdenlazaro@gmail.com>
2025-04-05 17:04:50 +0800

    Save progress to array methods

--- scripts/main.js
+++ scripts/main.js
@@ -19,4 +19,32 @@ const assortedArray = [
   someFunction,
   ["array", "array1"],
   null,
+  NaN,
 ];
+
+const array = assortedArray.sort();
+console.log(assortedArray.length);
+console.log(array);
+
+array.push({ firstname: "ded", lastname: "bol" });
+
+console.log(array);
+
+array.forEach((element) => {
+  let count = 0;
+  console.log(`${element}+${count}`);
+  count++;
+  console.log(count);
+});
+
+let thisArray = [];
+console.log(`previous thisArray.length: ${thisArray.length}`);
+
+for (let i = 0; i < 10; i++) {
+  thisArray.push(i);
+}
+
+console.log(`thisArray.length: ${array.length}`);
+for (let i = 0; i < array.length; i++) {
+  console.log(thisArray[i]);
+}
