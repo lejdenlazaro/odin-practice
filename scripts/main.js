c2b525bba0a5137b00ac7666ba0527ecb52e9296
Lejden Lazaro <lejdenlazaro@gmail.com>
2025-04-05 17:05:11 +0800

    Replace header title

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,8 @@
 "use strict";
 
+const header = document.querySelector("h1");
+header.textContent = "Fuck JavaScript";
+
 const div = document.querySelector("div");
 div.textContent = "🖕🏻";
 div.style.fontSize = "150px";
