9924dd54a8c2a39bffe7533c22fbb120eb15c4b4
Lejden Lazaro <lejdenlazaro@gmail.com>
2025-04-09 14:08:53 +0800

    Reset main script

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,1 @@
 "use strict";
-
-const header = document.querySelector("h1");
-header.textContent = "Fuck JavaScript";
-
-const div = document.querySelector("div");
-div.textContent = "🖕🏻";
-div.style.fontSize = "150px";
-
-//edit below
