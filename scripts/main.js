eef4d3c20cc85a97b6a91e77a33d03eb5a6d29dd
Lejden Lazaro <lejdenlazaro@gmail.com>
2025-04-11 19:31:58 +0800

    Comment BOM methods

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,13 @@
 "use strict";
+
+// function sayHi(){
+//     const input = prompt("Enter your name: ");
+// if (input !== null) {
+//   if (input === "") alert("You entered nothing!");
+//   else {
+//     alert(`Hi ${input}!`);
+//   }
+// } else {
+//   alert("Cancelled.");
+// }
+// }
