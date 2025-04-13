70af08a3562f3719c0cd4571d174a12bd6973d10
Lejden Lazaro <lejdenlazaro@gmail.com>
2025-04-13 16:41:42 +0800

    Add solution to DOM manipulation exercise

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,30 @@
 "use strict";
+
+const body = document.querySelector("body");
+
+//inside body
+const p = document.createElement("p");
+p.textContent = "Hey I'm red!";
+p.style.color = "red";
+
+const h3 = document.createElement("h3");
+h3.textContent = "I'm a blue h3!";
+h3.style.color = "blue";
+
+const div = document.createElement("div");
+div.style.border = "3px solid black";
+div.style.backgroundColor = "pink";
+
+body.appendChild(p);
+body.appendChild(h3);
+body.appendChild(div);
+
+// inside body div
+const h1 = document.createElement("h1");
+h1.textContent = "I'm in a div!";
+
+const p1 = document.createElement("p");
+p1.textContent = "ME TOO!";
+
+div.appendChild(h1);
+div.appendChild(p1);
